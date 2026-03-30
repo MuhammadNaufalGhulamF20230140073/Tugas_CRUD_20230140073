02e58de65c365bde552c75595fdafece4f54d137
Muhammad Naufal Ghulam F <naufalfakhrudddin@gmail.com>
2026-03-31 01:27:36 +0700

    Membuat KtpController untuk endpoint post

---
+++ src/main/java/com/example/demo/controller/KtpController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.KtpDto;
+import com.example.demo.service.KtpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ktp")
+public class KtpController {
+
+    @Autowired
+    private KtpService ktpService;
+
+    // POST: Tambah KTP
+    @PostMapping
+    public ResponseEntity<?> createKtp(@RequestBody KtpDto ktpDto) {
+        try {
+            KtpDto savedKtp = ktpService.createKtp(ktpDto);
+            return new ResponseEntity<>(savedKtp, HttpStatus.CREATED);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // GET: Ambil seluruh data
+    @GetMapping
+    public ResponseEntity<List<KtpDto>> getAllKtp() {
+        return ResponseEntity.ok(ktpService.getAllKtp());
+    }
+
